a76f6231410a45349db61d44e9744a372bac3f46
Roro Cui <robearro@gmail.com>
2021-06-08 21:36:27 -0400

    Total:73
    Update correct code of first unique character in a string
    
    Change Contents:
        None
    
    Signed-off-by: robearro@gmail.com

--- c_code/hash_table/first_unique_character_in_a_string.hashmap-array-bucket.c
+++ c_code/hash_table/first_unique_character_in_a_string.hashmap-array-bucket.c
@@ -1,11 +1,10 @@
 #define SIZE        26
 //#define DELTA 1000000000
 //#define BUCKET_SIZE 10000
-#define INIT_NUM    0
+#define INIT_NUM    -1
 #define ERROR_CODE  0x80000000
 #define MIN_INT_32  0x80000000
 #define MAX_INT     100001
-#define INIT        -1
 
 typedef struct {
     int* ptr;
@@ -39,14 +38,15 @@ int myHashMapGet(MyHashMap* obj, int key)
 }
 
 /** Count value accounting key and current count*/
-void myHashMapPut(MyHashMap* obj, int key, int val) 
+void myHashMapPut(MyHashMap* obj, char key, int val) 
 {
+    int index = key - 0x61;    
     /* update val(index) to first accurrance charator */
-    if(obj->ptr[key] == 0)
-        obj->ptr[key] = val;
+    if(obj->ptr[index] == INIT_NUM)
+        obj->ptr[index] = val;
     else
         /*if accurance happens, set mapping val to MIN_INT_32 for Not used flag */
-        obj->ptr[key] = MIN_INT_32;
+        obj->ptr[index] = MIN_INT_32;
 }
 
 /** find minimium index(val) in current array, except 0 and MIN_INT_32 **/
@@ -57,7 +57,8 @@ int myHashMapFindMinVal(MyHashMap* obj)
 
     for(i = 0; i < SIZE; i++)
     {
-        if(obj->ptr[i] != 0 && obj->ptr[i] != MIN_INT_32)
+        //printf("%d:%d\n", i, obj->ptr[i]);
+        if(obj->ptr[i] != INIT_NUM && obj->ptr[i] != MIN_INT_32)
             ret = ret < obj->ptr[i] ? ret : obj->ptr[i];
     }
 
@@ -81,19 +82,21 @@ int firstUniqChar(char *s)
 {
     MyHashMap* hash_new = NULL;
     int i;
-    int ret = INIT;
+    int ret = INIT_NUM;
 
     hash_new = myHashMapCreate();
+    //printf("%s:0x%x\n", __func__, hash_new);
     for(i = 0; i < strlen(s); i++)
     {
+        //printf("[%d]:%c\n", i, s[i]);
         myHashMapPut(hash_new, s[i], i);
     }
 
     ret = myHashMapFindMinVal(hash_new);
     myHashMapFree(hash_new);
 
-    return ret;
+    if(ret == MAX_INT) return -1;
+    else return ret;
 }
 
-/* 36-164(<26%)-187.9 , on May 30th, 2021 */
-
+/* 104-272-7, on Jun 8th, 2021 */

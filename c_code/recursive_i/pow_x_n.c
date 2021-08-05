eadabb3a6ceaeb9c3931b96285f17d34af8069d0
Roro Cui <robearro@gmail.com>
2021-08-04 22:26:54 -0400

    Total: 84
    update another method of power(x,n)
    
    Change Contents:
        None
    
    Signed-off-by: robearro@gmail.com

--- c_code/recursive_i/pow_x_n.c
+++ c_code/recursive_i/pow_x_n.c
@@ -1,38 +1,29 @@
-/* Time limit exceeded for tail recursion method
-double pow_tail_recursion(double x, double base, long n)
+double pow_tail_recursion(double x, long n)
 {
-   if(n == 0) return 1.0; 
+   double ret = 0.0; 
+    
    if(n == 1) return x;
-   return pow_tail_recursion(x * base, base, n - 1);
-}
-
-long pow_tail_recursion_int(long x, int base, long n)
-{
-   printf("%ld,%ld\n", x, n);
-   if(n == 0) return 1; 
-   if(n == 1) return x;
-   return pow_tail_recursion_int(x * base, base, n - 1);
+   if(n % 2 == 0) 
+   {
+       ret = pow_tail_recursion(x, n / 2);
+       return ret * ret; 
+   }
+    else
+    {
+       return pow_tail_recursion(x, n / 2) * pow_tail_recursion(x, n - n / 2); 
+    }
 }
 
 double myPow(double x, long n)
 {
-    double ret;
-    long   x_int;
-    
+    if(n == 0) return 1.0; 
     if(x == 1) return 1.0;
-    x_int = (long)x;
-    printf("%ld,%f\n", x_int, x);
-    if(x_int == x) 
-        ret = (double)pow_tail_recursion_int(x_int, (int)x_int, labs(n));
-    else
-        ret = pow_tail_recursion(x, x, labs(n));
-    //ret = pow_tail_recursion(x, x, labs(n));
-    if(n < 0)
-      return  1 / ret;
-    else
-      return  ret;
+    if(n < 0) x = 1/x; 
+    
+    return pow_tail_recursion(x, labs(n));
 }
-*/
+/* 304 - 0(<100%) - 5.5, on Aug 4st, 2021 */
+/*
 
 double myPow(double x, long n)
 {
@@ -58,5 +49,6 @@ double myPow(double x, long n)
     }
 }
 
+*/
 /* Non tail recursion method */
 /* 304 - 0(<100%) - 5.4, on Aug 1st, 2021 */

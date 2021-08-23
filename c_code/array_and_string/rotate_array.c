d2b153aa55b82d6429de94b33293e004e523a2b3
Roro Cui <robearro@gmail.com>
2021-08-22 23:03:13 -0400

    Total:100
    Remove non necessary if(numsSize == 1) in Rotate array quiz
    
    Change Contents:
        None
    
    Signed-off-by: robearro@gmail.com

--- c_code/array_and_string/rotate_array.c
+++ c_code/array_and_string/rotate_array.c
@@ -3,19 +3,17 @@ void rotate(int* nums, int numsSize, int k)
     int* temp = NULL;
     int i;
 
-    if(numsSize != 1)
-    {
-        k = k % numsSize;
-        temp = (int*)calloc(numsSize - k, sizeof(int));
-        for(i = 0; i < (numsSize - k); i++)
-            temp[i] = nums[i];
-        for(i = 0; i < k; i++)
-            nums[i] = nums[i + numsSize - k];
-        for(i = k; i < numsSize; i++)
-            nums[i] = temp[i - k];
+    k = k % numsSize;
+    temp = (int*)calloc(numsSize - k, sizeof(int));
+    for(i = 0; i < (numsSize - k); i++)
+        temp[i] = nums[i];
+    for(i = 0; i < k; i++)
+        nums[i] = nums[i + numsSize - k];
+    for(i = k; i < numsSize; i++)
+        nums[i] = temp[i - k];
 
-        free(temp);
-    }
+    free(temp);
 }
 
 /* 37 - 108ms(<65%) - 22.5MB, on Aug 22nd, 2021 */
+/* no if (numsSize == 1) 37 - 100ms(<94%) - 22.6MB, on Aug 22nd, 2021 */

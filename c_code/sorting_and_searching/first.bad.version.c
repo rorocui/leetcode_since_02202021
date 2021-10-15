87f8d583d69cc9229b7b310934e1c038c440043b
Roro Cui <robearro@gmail.com>
2021-10-14 22:48:43 -0400

    Total:113
    Complete Find first bad version
    
    Change Contents:
        None
    
    Signed-off-by: robearro@gmail.com

---
+++ c_code/sorting_and_searching/first.bad.version.c
@@ -0,0 +1,37 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+
+int firstBadVersion(int n) 
+{
+    int bad;
+    long min, max;
+
+    bad = n / 2;
+    min = 0;
+    max = n;
+    while(bad != n || bad != 1)
+    {
+        //printf("bad=%d,%d\n", bad, isBadVersion(bad));
+        if(isBadVersion(bad))
+            if(isBadVersion(bad - 1))
+            {
+                max = bad;
+                bad = (int)((min + max) / 2);
+            }
+            else
+                return bad;
+        else
+            if(isBadVersion(bad + 1))
+                return bad + 1;
+            else
+            {
+                min = bad;
+                bad = (int)((min + max) / 2);
+            }
+    }
+    return bad;
+
+}
+/* 22 - 3(<18%) - 5.5MB, on Oct 14th, 2021 */
+

3095f19b542a88e425a2717213c3642021476482
Roro Cui <robearro@gmail.com>
2022-01-15 22:27:31 -0500

    Total: 132(unchanged)
    Complete the Find Peak Element with O(log n) algorithm.
    
    Change Contents:
        None
    
    Signed-off-by: robearro@gmail.com

--- c_code/binary_search/find.peak.element.c
+++ c_code/binary_search/find.peak.element.c
@@ -1,52 +1,27 @@
-int find_peak_recursion(int* nums, int start, int end, int* idx)
-{
-    int mid;
-    int i;
-    int ret;
-    int idx_top = -1, idx_bottom = -1;
-    int ret_top, ret_bottom;
-
-
-    if((end - start) < 3)
-    {
-        ret = nums[start];
-        *idx = start;
-        for(i = start + 1; i <= end; i++)
-        {
-            if(ret < nums[i]) 
-            {
-                ret = nums[i];
-                *idx = i;
-            }
-        }
-        return ret;
-    }
-    mid = start + (end - start) / 2;
-    ret_top = find_peak_recursion(nums, start, mid, &idx_top); 
-    ret_bottom = find_peak_recursion(nums, mid + 1, end, &idx_bottom);
-    if(ret_top < ret_bottom)
-    {
-        ret = ret_bottom;
-        *idx = idx_bottom;
-    }
-    else
-    {
-        ret = ret_top;
-        *idx = idx_top;
-    }
-
-    return ret;
-
-}
-
 int findPeakElement(int* nums, int numsSize)
 {
-    int ret_idx;
+    int start, end, mid;
 
-    find_peak_recursion(nums, 0, numsSize - 1, &ret_idx);
-
-    return ret_idx;
-
+    start = 0;
+    end = numsSize - 1;
+    mid = start + (end - start) / 2;
+    while(start < end)
+    {
+        if(start == mid) return nums[mid] > nums[end] ? mid : end;
+        if(nums[mid] < nums[mid + 1])
+        {
+            start = mid + 1;
+        }
+        else if(nums[mid - 1] > nums[mid])
+        {
+            end = mid - 1;
+        }
+        /* mush be this condition (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) */
+        else
+            return mid;
+        mid = start + (end - start) / 2;
+    }
+    /* should not reach here to be complete, add return failure here. */
+    return start; 
 }
-
-/* 63 - 3(<89%) - 5.9MB, on Jan 15th, 2022 */
+/* 63 - 0(<100%) - 5.7MB, on Jan 15th, 2022 */
